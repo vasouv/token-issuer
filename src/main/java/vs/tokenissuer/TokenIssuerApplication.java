ed78dcbf167413fd77cf74f47c601d480d064b36
Vasilis Souvatzis <vasouv@gmail.com>
2019-03-19 22:17:36 +0200

    Initial commit

---
+++ src/main/java/vs/tokenissuer/TokenIssuerApplication.java
@@ -0,0 +1,13 @@
+package vs.tokenissuer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenIssuerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenIssuerApplication.class, args);
+	}
+
+}
